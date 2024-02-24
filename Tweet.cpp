71e6b3a594c10fa3573e13d2c2ab7320664d68d4
aaronvsc <aaronvsc@outlook.com>
2024-02-23 20:57:44 -0600

    Tweet functions

--- Tweet.cpp
+++ Tweet.cpp
@@ -5,14 +5,17 @@ Tweet::Tweet() {}
 
 // Constructor with parameters
 Tweet::Tweet(const DSString& tweetID, const DSString& text) {
-    
+    this->tweetID = tweetID;
+    this->text = text;
 }
 
 // Getter for tweet ID
 DSString Tweet::getTweetID() const {
+    return tweetID;
 }
 
 // Getter for tweet text
 DSString Tweet::getText() const {
+    return text;
 }
 
